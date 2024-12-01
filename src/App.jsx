b974424a55353a2c730c6405eb6c9023724aa7a2
nmsguerrero <guerreroninamarie39@gmail.com>
2024-12-01 20:41:09 +0800

    Added the grid columns for food items of the website.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { FaSearch, FaPhone } from "react-icons/fa";
+import { FaSearch, FaPhone, FaMinusCircle, FaPlusCircle } from "react-icons/fa";
 
 export default function App() {
   return (
@@ -15,7 +15,7 @@ export default function App() {
             <li className="inline-block px-3">Delivery</li>
             <li className="inline-block px-3">Contacts</li>
             <li className="inline-block px-3">
-              <FaSearch className="text-xl "></FaSearch>
+              <FaSearch className="text-xl"></FaSearch>
             </li>
           </ul>
         </nav>
@@ -41,7 +41,202 @@ export default function App() {
           </div>
         </div>
       </section>
-      <main></main>
+      <main className="px-56">
+        <div className="grid grid-cols-3 gap-3">
+          <div className="shadow-lg w-72 flex flex-col justify-center items-center align-center py-4 px-6 text-center">
+            <img className="w-48" src="../src/assets/images/dish/pizza-1.png" />
+            <h2 className="font-bold text-2xl">Pizza Margherita</h2>
+            <p className="text-base my-2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit
+            </p>
+            <div className="flex justify-between w-40 font-bold">
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                25
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                30
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                42
+              </div>
+            </div>
+            <div className="flex w-64 justify-between my-4">
+              <p className="font-bold text-2xl">10 000&#36;</p>
+              <div className="flex items-center">
+                <FaMinusCircle></FaMinusCircle>
+                <input
+                  type="text"
+                  className="w-7 mx-0.5 bg-slate-400 rounded text-white"
+                />
+                <FaPlusCircle></FaPlusCircle>
+              </div>
+            </div>
+            <button className="bg-red-600 py-2 px-10 rounded text-white uppercase font-bold">
+              Buy
+            </button>
+          </div>
+          <div className="shadow-lg w-72 flex flex-col justify-center items-center align-center py-4 px-6 text-center">
+            <img className="w-48" src="../src/assets/images/dish/pizza-1.png" />
+            <h2 className="font-bold text-2xl">Pizza Margherita</h2>
+            <p className="text-base my-2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit
+            </p>
+            <div className="flex justify-between w-40 font-bold">
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                25
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                30
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                42
+              </div>
+            </div>
+            <div className="flex w-64 justify-between my-4">
+              <p className="font-bold text-2xl">10 000&#36;</p>
+              <div className="flex items-center">
+                <FaMinusCircle></FaMinusCircle>
+                <input
+                  type="text"
+                  className="w-7 mx-0.5 bg-slate-400 rounded text-white"
+                />
+                <FaPlusCircle></FaPlusCircle>
+              </div>
+            </div>
+            <button className="bg-red-600 py-2 px-10 rounded text-white uppercase font-bold">
+              Buy
+            </button>
+          </div>
+          <div className="shadow-lg w-72 flex flex-col justify-center items-center align-center py-4 px-6 text-center">
+            <img className="w-48" src="../src/assets/images/dish/pizza-1.png" />
+            <h2 className="font-bold text-2xl">Pizza Margherita</h2>
+            <p className="text-base my-2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit
+            </p>
+            <div className="flex justify-between w-40 font-bold">
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                25
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                30
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                42
+              </div>
+            </div>
+            <div className="flex w-64 justify-between my-4">
+              <p className="font-bold text-2xl">10 000&#36;</p>
+              <div className="flex items-center">
+                <FaMinusCircle></FaMinusCircle>
+                <input
+                  type="text"
+                  className="w-7 mx-0.5 bg-slate-400 rounded text-white"
+                />
+                <FaPlusCircle></FaPlusCircle>
+              </div>
+            </div>
+            <button className="bg-red-600 py-2 px-10 rounded text-white uppercase font-bold">
+              Buy
+            </button>
+          </div>
+          <div className="shadow-lg w-72 flex flex-col justify-center items-center align-center py-4 px-6 text-center">
+            <img className="w-48" src="../src/assets/images/dish/pizza-1.png" />
+            <h2 className="font-bold text-2xl">Pizza Margherita</h2>
+            <p className="text-base my-2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit
+            </p>
+            <div className="flex justify-between w-40 font-bold">
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                25
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                30
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                42
+              </div>
+            </div>
+            <div className="flex w-64 justify-between my-4">
+              <p className="font-bold text-2xl">10 000&#36;</p>
+              <div className="flex items-center">
+                <FaMinusCircle></FaMinusCircle>
+                <input
+                  type="text"
+                  className="w-7 mx-0.5 bg-slate-400 rounded text-white"
+                />
+                <FaPlusCircle></FaPlusCircle>
+              </div>
+            </div>
+            <button className="bg-red-600 py-2 px-10 rounded text-white uppercase font-bold">
+              Buy
+            </button>
+          </div>
+          <div className="shadow-lg w-72 flex flex-col justify-center items-center align-center py-4 px-6 text-center">
+            <img className="w-48" src="../src/assets/images/dish/pizza-1.png" />
+            <h2 className="font-bold text-2xl">Pizza Margherita</h2>
+            <p className="text-base my-2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit
+            </p>
+            <div className="flex justify-between w-40 font-bold">
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                25
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                30
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                42
+              </div>
+            </div>
+            <div className="flex w-64 justify-between my-4">
+              <p className="font-bold text-2xl">10 000&#36;</p>
+              <div className="flex items-center">
+                <FaMinusCircle></FaMinusCircle>
+                <input
+                  type="text"
+                  className="w-7 mx-0.5 bg-slate-400 rounded text-white"
+                />
+                <FaPlusCircle></FaPlusCircle>
+              </div>
+            </div>
+            <button className="bg-red-600 py-2 px-10 rounded text-white uppercase font-bold">
+              Buy
+            </button>
+          </div>
+          <div className="shadow-lg w-72 flex flex-col justify-center items-center align-center py-4 px-6 text-center">
+            <img className="w-48" src="../src/assets/images/dish/pizza-1.png" />
+            <h2 className="font-bold text-2xl">Pizza Margherita</h2>
+            <p className="text-base my-2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit
+            </p>
+            <div className="flex justify-between w-40 font-bold">
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                25
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                30
+              </div>
+              <div className="border-2 rounded-full px-2 py-1 border-red-400 text-red-400">
+                42
+              </div>
+            </div>
+            <div className="flex w-64 justify-between my-4">
+              <p className="font-bold text-2xl">10 000&#36;</p>
+              <div className="flex items-center">
+                <FaMinusCircle></FaMinusCircle>
+                <input
+                  type="text"
+                  className="w-7 mx-0.5 bg-slate-400 rounded text-white"
+                />
+                <FaPlusCircle></FaPlusCircle>
+              </div>
+            </div>
+            <button className="bg-red-600 py-2 px-10 rounded text-white uppercase font-bold">
+              Buy
+            </button>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
