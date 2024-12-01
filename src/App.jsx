960129e2e74242af1b25065ab78398eb0cea9be8
nmsguerrero <guerreroninamarie39@gmail.com>
2024-12-01 11:19:50 +0800

    Used tailwind's utility-first approach in HTML elements.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaPhone } from "react-icons/fa";
 
 export default function App() {
   return (
     <>
       <header className="bg-black text-white text-center h-12 flex justify-between items-center px-56">
-        <div className="logo-style">Italian Pizza</div>
+        <div className="font-play-hr italic text-2xl">Italian Pizza</div>
         <nav>
-          <ul className="navbar">
-            <li>Pizza</li>
-            <li>Burgers</li>
-            <li>Fries</li>
-            <li>Sauce</li>
-            <li>Drinks</li>
-            <li>Delivery</li>
-            <li>Contacts</li>
-            <li>
+          <ul className="uppercase">
+            <li className="inline-block px-3">Pizza</li>
+            <li className="inline-block px-3">Burgers</li>
+            <li className="inline-block px-3">Fries</li>
+            <li className="inline-block px-3">Sauce</li>
+            <li className="inline-block px-3">Drinks</li>
+            <li className="inline-block px-3">Delivery</li>
+            <li className="inline-block px-3">Contacts</li>
+            <li className="inline-block px-3">
               <FaSearch className="text-xl "></FaSearch>
             </li>
           </ul>
         </nav>
       </header>
+      <section className="bg-hero-section bg-center bg-cover bg-no-repeat bg-fixed relative h-1/2">
+        <div>
+          <img src="../src/assets/images/pizza-chef.jpg" className="w-40" />
+        </div>
+        <div>
+          <p>123456, Florida</p>
+          <p>Lorem ipsum</p>
+          <p>0(000) 000-00-00</p>
+          <button>
+            <FaPhone></FaPhone>
+            Order Bell
+          </button>
+        </div>
+      </section>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "play-hr": ["'Playwrite HR Lijeva'", "cursive"],
+      },
+      backgroundImage: {
+        "hero-section": "url('../src/assets/images/background-image.avif')",
+      },
+    },
   },
   plugins: [],
 };
